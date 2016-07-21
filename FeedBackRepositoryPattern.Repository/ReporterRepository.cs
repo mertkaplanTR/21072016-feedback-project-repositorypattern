5deee0df3df4d3d38f6c470ee7e2d7ad13bd68d3
mertkaplan_tr <mertkaplan.tr@gmail.com>
2016-07-21 17:32:11 +0300

    first day v2

---
+++ FeedBackRepositoryPattern.Repository/ReporterRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FeedBackRepositoryPattern.ORM;
+namespace FeedBackRepositoryPattern.Repository
+{
+    public class ReporterRepository:RepositoryBase<Reporter>
+    {
+        public void AddReport(Reporter p1, Reporter p2, Reporter p3, Reporter p4, Reporter p5, Reporter p6)
+        {
+            //start there
+        }
+    }
+}
