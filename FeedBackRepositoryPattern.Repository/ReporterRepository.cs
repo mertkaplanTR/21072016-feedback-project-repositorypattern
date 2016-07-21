2bbba38056d97030019050f95d020dc6f9b2bd00
mertkaplan_tr <mertkaplan.tr@gmail.com>
2016-07-21 17:33:04 +0300

    v3

--- FeedBackRepositoryPattern.Repository/ReporterRepository.cs
+++ FeedBackRepositoryPattern.Repository/ReporterRepository.cs
@@ -7,7 +7,7 @@ namespace FeedBackRepositoryPattern.Repository
 {
     public class ReporterRepository:RepositoryBase<Reporter>
     {
-        public void AddReport(Reporter p1, Reporter p2, Reporter p3, Reporter p4, Reporter p5, Reporter p6)
+        public void AddReport(Reporter p1, Reporter p2, Reporter p3, Reporter p4, Reporter p5)
         {
             //start there
         }
